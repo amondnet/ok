801529f7799ee25724e171a6e48a43f86fca9b37
Minsu Lee <amond@amond.net>
2020-05-19 14:15:15 +0900

    first commit

---
+++ lib/ok.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library ok;
+
+export 'src/ok_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
